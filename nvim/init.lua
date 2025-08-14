8c24f37496d576839b975a76b1aa45914b15458e
newbiedeveloper968 <newbiedeveloper968@gmail.com>
2025-08-15 00:47:57 +0600

    Fixed nvim lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,21 +3,21 @@
 -- ========================
 
 vim.opt.number = true                  -- Show absolute line numbers
-vim.opt.relativenumber = true         -- Show relative line numbers
-vim.opt.expandtab = true              -- Use spaces instead of tabs
-vim.opt.shiftwidth = 4                -- Indent width
-vim.opt.tabstop = 4                   -- Tab character width
-vim.opt.smartindent = true            -- Smarter autoindenting
-vim.opt.termguicolors = true          -- Enable 24-bit RGB colors
-vim.opt.cursorline = true             -- Highlight the line under cursor
-vim.opt.clipboard = "unnamedplus"     -- Sync with system clipboard
-vim.opt.mouse = "a"                   -- Enable mouse support
-vim.opt.wrap = true                   -- Enable line wrapping
-vim.opt.swapfile = false              -- Disable swapfile
+vim.opt.relativenumber = true           -- Show relative line numbers
+vim.opt.expandtab = true                -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4                  -- Indent width
+vim.opt.tabstop = 4                     -- Tab character width
+vim.opt.smartindent = true               -- Smarter autoindenting
+vim.opt.termguicolors = true             -- Enable 24-bit RGB colors
+vim.opt.cursorline = true                -- Highlight the line under cursor
+vim.opt.clipboard = "unnamedplus"        -- Sync with system clipboard
+vim.opt.mouse = "a"                      -- Enable mouse support
+vim.opt.wrap = true                      -- Enable line wrapping
+vim.opt.swapfile = false                 -- Disable swapfile
 
-vim.opt.timeoutlen = 300              -- Timeout for key sequences
+vim.opt.timeoutlen = 300                 -- Timeout for key sequences
 
-vim.g.mapleader = " "                 -- Leader key set to space
+vim.g.mapleader = " "                    -- Leader key set to space
 
 -- ========================
 --       PLUGINS
@@ -34,9 +34,12 @@ Plug 'nvim-lua/plenary.nvim'           " Required by telescope
 Plug 'neovim/nvim-lspconfig'           " LSP configurations
 Plug 'hrsh7th/nvim-cmp'                " Autocomplete plugin
 Plug 'hrsh7th/cmp-nvim-lsp'            " LSP source for autocomplete
+Plug 'hrsh7th/cmp-buffer'              " Buffer words source
+Plug 'hrsh7th/cmp-path'                " Filesystem paths source
+Plug 'hrsh7th/cmp-cmdline'             " Command-line completion
 Plug 'L3MON4D3/LuaSnip'                " Snippet engine
 Plug 'saadparwaiz1/cmp_luasnip'        " Snippet completions
-Plug 'windwp/nvim-autopairs'
+Plug 'windwp/nvim-autopairs'           " Auto close pairs
 
 call plug#end()
 ]]
@@ -46,55 +49,84 @@ call plug#end()
 -- ========================
 
 local actions = require('telescope.actions')
-local telescope = require('telescope')
-
-telescope.setup{
+require('telescope').setup{
   defaults = {
     mappings = {
-      i = { ["<CR>"] = actions.select_vertical },  -- Insert mode: open in vsplit
-      n = { ["<CR>"] = actions.select_vertical },  -- Normal mode: open in vsplit
+      i = { ["<CR>"] = actions.select_vertical },
+      n = { ["<CR>"] = actions.select_vertical },
     },
   },
 }
 
 -- ========================
---   AUTOPAIRS + CMP
+--   CMP + AUTOPAIRS SETUP
 -- ========================
 
-require('nvim-autopairs').setup{}
-
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
+local luasnip = require('luasnip')
 
-cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
-)
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+    { name = 'path' },
+  })
+})
+
+require('nvim-autopairs').setup{}
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 
 -- ========================
 --      KEYBINDINGS
 -- ========================
 
--- Copy path to clipboard
 vim.keymap.set("n", "<leader>cp", [[:let @+ = expand('%:p:h')<CR>]], { noremap = true, silent = true })
 
--- Theme
 vim.cmd("colorscheme vscode")
 -- vim.cmd("colorscheme gruvbox")
 
--- Exit insert mode with 'kj'
 vim.keymap.set("i", "kj", "<ESC>", { noremap = true, silent = true })
 
--- Yank to system clipboard
 vim.keymap.set("v", "y", '"+y', { noremap = true, silent = true })
 vim.keymap.set("n", "yy", '"+yy', { noremap = true, silent = true })
 
--- NERDTree toggle and settings
 vim.keymap.set("n", "<leader>e", ":NERDTreeFind<CR>", { noremap = true, silent = true })
 vim.g.NERDTreeChDirMode = 2
 vim.g.NERDTreeWinSize = 20
 
--- Telescope bindings
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { silent = true })
 vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { silent = true })
 
@@ -105,7 +137,6 @@ vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { silent = true })
 local lspconfig = require("lspconfig")
 local servers = { "pyright", "clangd" }
 
--- Setup LSP keybinds when attached to buffer
 local on_attach = function(_, bufnr)
   local opts = { noremap=true, silent=true, buffer=bufnr }
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
@@ -118,25 +149,9 @@ local on_attach = function(_, bufnr)
   vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
 end
 
--- Setup each LSP server
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
     capabilities = require("cmp_nvim_lsp").default_capabilities(),
   }
 end
-
--- ========================
---    OPTIONAL: Autoformat
--- ========================
---[[
-if vim.fn.executable("clang-format") == 1 then
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = { "*.c", "*.h", "*.cpp", "*.hpp" },
-    callback = function()
-      vim.cmd("%!clang-format")
-    end,
-    desc = "Auto format C/C++ with clang-format on save",
-  })
-end
-]]
